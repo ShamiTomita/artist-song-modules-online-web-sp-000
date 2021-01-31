4e3c23add9681e0145d474bb48dec89f0c4d0cc9
Shami Tomita <shamitomita@gmail.com>
2021-01-31 20:27:34 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,10 @@
 module Findable 
 def find_by_name(name)
-   detect{|a| a.name == name}
+   self.detect{|a| a.name == name}
  end 
   
   def find_by_name(name)
-    detect{|a| a.name == name}
+    selfdetect{|a| a.name == name}
   end
   
 end 
